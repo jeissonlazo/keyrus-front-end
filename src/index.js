e8551cf20a45ea2027efcddcf6e66aa90efbe7bf
jeisson lazo <jeisson.lazo@apriorins.com>
2020-02-28 09:02:34 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './mis/css/bootstrap.css';
+import './styles/main.scss';
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+import 'bootstrap';
+import './mis/icons/style.css';
+import 'animate.css';
+//document.querySelector('h1').textContent = bro('how is going');
+console.log($('#successAlert').text())
